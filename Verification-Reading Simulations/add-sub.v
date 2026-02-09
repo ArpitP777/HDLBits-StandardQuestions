53c3d3305e6020523244b3b10e9672520e4461dc
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-02-10 00:23:44 +0530

    added add-sub soln

---
+++ Verification-Reading Simulations/add-sub.v
@@ -0,0 +1,19 @@
+// synthesis verilog_input_version verilog_2001
+module top_module ( 
+    input do_sub,
+    input [7:0] a,
+    input [7:0] b,
+    output reg [7:0] out,
+    output reg result_is_zero
+);//
+
+    always @(*) begin
+        if(do_sub)
+            out = a-b;
+        else
+            out = a+b;
+        
+         result_is_zero = !out;
+    end
+
+endmodule
