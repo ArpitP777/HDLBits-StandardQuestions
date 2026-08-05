347983f6680c98f832aca3edde03894d66b93b1c
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-08-06 02:13:08 +0530

    added tb soln

---
+++ Verification-Reading Simulations/testbench1.v
@@ -0,0 +1,21 @@
+module top_module ( output reg A, output reg B );//
+
+    // generate input patterns here
+    initial begin
+		A = 0;
+        B = 0;
+        #10;
+        A = 1;
+        B = 0;
+        #5;
+        B = 1;
+        A = 1;
+        #5;
+        A = 0;
+        B = 1;
+        #20;
+        B = 0;
+        
+    end
+
+endmodule
