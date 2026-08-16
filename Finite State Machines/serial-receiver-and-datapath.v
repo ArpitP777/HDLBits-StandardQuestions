cb3384c8ffe3e9490eea237955c436a5688f9cbc
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-08-16 17:36:58 +0530

    added FSM sol

---
+++ Finite State Machines/serial-receiver-and-datapath.v
@@ -0,0 +1,67 @@
+module top_module(
+    input clk,
+    input in,
+    output reg [7:0] out_byte,
+    input reset,    // Synchronous reset
+    output done
+); 
+    parameter [2:0] IDLE=3'd0, DATA=3'd1, STOP=3'd2, ERROR=3'd3, DONE=3'd4;
+	
+    reg [3:0] count;
+    reg [2:0] ps,ns;
+    
+    always@(posedge clk) begin
+        if(reset) begin
+           ps <= IDLE; 
+           count <= 4'd0;
+        end
+        else begin
+            if(ps == DATA) begin
+                out_byte <= {in , out_byte[7:1]};
+           		count <= count + 1;
+        	end
+            else begin
+               	count <= 4'd0;
+            end
+       		ps <= ns; 
+        end
+    end
+    
+    always@(*) begin
+        case(ps)
+            IDLE: if(in == 1'b0) begin
+                ns = DATA;
+            end
+            else begin
+               ns = IDLE;
+            end
+            DATA: if(count == 4'd7) begin
+               		ns = STOP; 
+            	end
+            else begin
+               ns = DATA; 
+            end
+            STOP: if(in == 1'b1) begin
+                ns = DONE;
+            end
+            else begin
+               ns = ERROR; 
+            end
+            DONE: if(in == 1'b0) begin
+            		ns = DATA;    
+                end
+            	  else begin
+                    ns <= IDLE;
+                  end
+            ERROR: if(in == 1'b1) begin
+               ns = IDLE; 
+            end
+            else begin
+               ns = ERROR;
+            end
+            default: ns = IDLE;
+        endcase
+    end
+    
+    assign done = (ps==DONE);
+endmodule
